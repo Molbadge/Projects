d002c5c45d94b4ffc817e1c4ebacff5b44873239
Molly Badger <mrb9856@rit.edu>
2023-09-03 13:28:53 -0400

    Consolidated JS into one file

--- ButtonDemo/src/login.js
+++
@@ -1,3 +0,0 @@
-function toMain() {
-    window.location.href = "main.html";
-  }
--- ButtonDemo/src/main.js
+++ ButtonDemo/src/main.js
@@ -4,3 +4,7 @@ const navi = document.querySelector(".navbar");
 navMenu.addEventListener('click', ()=>{
     navi.classList.toggle('close')
 })
+
+function toMain() {
+    window.location.href = "main.html";
+}
