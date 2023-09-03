c1a17d25a245994a77690c650948f5abbbca629f
Molly Badger <mrb9856@rit.edu>
2023-09-03 12:52:17 -0400

    Added main page with basic menu

---
+++ ButtonDemo/src/main.js
@@ -0,0 +1,6 @@
+const navMenu = document.querySelector(".nav-menu");
+const navi = document.querySelector(".navbar");
+
+navMenu.addEventListener('click', ()=>{
+    navi.classList.toggle('close')
+})
